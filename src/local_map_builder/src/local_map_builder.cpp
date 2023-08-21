dc77b1aa662e9207c6334268838e92d7dca979cb
Kirill Lalayants <lalayantskirill@gmail.com>
2023-08-21 17:31:28 +0300

    paths

--- src/local_map_builder/src/local_map_builder.cpp
+++ src/local_map_builder/src/local_map_builder.cpp
@@ -2,7 +2,7 @@
 
 LocalMapBuilder::LocalMapBuilder(ros::NodeHandle & _node_handler){
     ROS_INFO("LocalMapBuilder creation started");
-    map_listener = node_handler.subscribe("/vl53l1x/range", 10, &LocalMapBuilder::set_forward_callback, this);
+    ros::Subscriber map_listener = node_handler.subscribe("/vl53l1x/range", 10, &LocalMapBuilder::set_forward_callback, this);
 }
 
 LocalMapBuilder::~LocalMapBuilder()
